e9d0ddd065c27a5651d118b2e97f2769f4a0a65b
zpremio <svenrus@live.de>
2014-11-28 19:53:44 +0100

    Rechen Fehler beseitigt...
    ...und Ausgabe Formatiert.

--- Program.cs
+++ Program.cs
@@ -12,13 +12,13 @@ namespace Zigarretenpreisrechner
         {
             while (true)
             {
-                const double Einzelpreis = 0.25;
+                const double Einzelpreis = 0.21;
                 double PreisProTag = 0, PreisProJahr = 0;
-                double KostenBisher = 0, Kosten65, KostenGesamt;
+                double KostenBisher = 0, Kosten65, bis65, KostenGesamt;
 
                 int alter = 0;
-                int anfangsaltereingabe, anfangsalter = 0, zwischenalter = 0;
-                int AnzahlProTagEingabe, AnzahlProTag = 0;
+                int anfangsalter = 0, zwischenalter = 0;
+                int AnzahlProTag = 0;
 
 
 
@@ -65,19 +65,18 @@ namespace Zigarretenpreisrechner
                 zwischenalter = alter - anfangsalter;
 
                 KostenBisher = zwischenalter * PreisProJahr;
-                Kosten65 = alter - 65 * PreisProJahr + KostenBisher;
+                bis65 = 65 - alter;
+                Kosten65 = bis65 * PreisProJahr;
                 KostenGesamt = KostenBisher + Kosten65;
 
-
-
-
-                //Debug
-                Console.WriteLine("PreisProTag: " + PreisProTag);
-                Console.WriteLine("PreisProJahr: " + PreisProJahr);
-                Console.WriteLine("ZwischenAlter: " + zwischenalter);
-                Console.WriteLine("KostenBisher: " + KostenBisher);
-                Console.WriteLine("Kosten65: " + Kosten65);
-                Console.WriteLine("KostenGesamt: " + KostenGesamt);
+                Console.WriteLine("");
+                Console.WriteLine("Das Rauchen kostet dich wenn eine Zigarette 0.21 EUR kostet: "); Console.WriteLine("");
+                Console.WriteLine("An Einem Tag: " + PreisProTag + " EUR"); Console.WriteLine("");
+                Console.WriteLine("In einem Jahr: " + PreisProJahr + " EUR"); Console.WriteLine("");
+                Console.WriteLine("Kosten bis Heute: " + KostenBisher + " EUR"); Console.WriteLine("");
+                Console.WriteLine("Kosten von Heute bis zum Alter von 65: " + Kosten65 + " EUR"); Console.WriteLine("");
+                Console.WriteLine("Kosten vom Anfang bis zum Alter von 65: " + KostenGesamt + " EUR");
+                Console.WriteLine("================================================================");
 
                 /*string nein, ja;
                 Console.WriteLine("Möchtest du eine neue Berechnung starten? (Y/N): ");*/
